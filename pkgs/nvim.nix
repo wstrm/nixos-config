7186d7bd8308c1624cbd1ec44416bd70a2fc1432
William Wennerström <william@willeponken.me>
2017-11-18 16:18:14 +0100

    nvim: add more plugins

--- pkgs/nvim.nix
+++ pkgs/nvim.nix
@@ -8,6 +8,16 @@
 
 		set number
 		set norelativenumber
+
+                set statusline+=%{fugitive#statusline()}
+                set statusline+=%#warningmsg#
+                set statusline+=%{SyntasticStatuslineFlag()}
+                set statusline+=%*
+
+                let g:syntastic_always_populate_loc_list = 1
+                let g:syntastic_auto_loc_list = 1
+                let g:syntastic_check_on_open = 1
+                let g:syntastic_check_on_wq = 0
 	'';
 	vam.pluginDictionaries = [
 		{
@@ -15,6 +25,13 @@
 				"vim-go"
                                 "vim-nix"
                                 "youcompleteme"
+                                "nerdtree"
+                                "syntastic"
+                                "tagbar"
+                                "deoplete-go"
+                                "goyo"
+                                "commentary"
+                                "fugitive"
 			];
 		}
 	];
